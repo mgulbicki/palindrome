fdff9757c357354976b1f968dccb5b3cd2801555
Marek Gulbicki <mgulbicki73@gmail.com>
2020-05-04 09:40:41 +0200

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
+module.exports = Phrase;
+
 // Adds `reverse` to all strings.
 String.prototype.reverse = function() {
-    return Array.from(this).reverse().join("");
-  }
+  return Array.from(this).reverse().join("");
+}
 
 // Defines a Phrase object.
 function Phrase(content) {
-    this.content = content;
+  this.content = content;
 
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+    return this.letters().toLowerCase();
   }
-  
-    // Returns true if the phrase is a palindrome, false otherwise.
-    this.palindrome = function palindrome() {
-        return this.processedContent() === this.processedContent().reverse();
-    }
+
+  // Returns the letters in the content.
+  // For example:
+  //   new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    return (this.content.match(/[a-z]/gi) || []).join("");
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
 }
-
-// Defines a TranslatedPhrase object.
-function TranslatedPhrase(content, translation) {
-    this.content = content;
-    this.translation = translation;
-
-    // Returns translation processed for palindrome testing.
-    this.processedContent = function processedContent() {
-        return this.translation.toLowerCase();
-    }
-}
-
-TranslatedPhrase.prototype = new Phrase();
